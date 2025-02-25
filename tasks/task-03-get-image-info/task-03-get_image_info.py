2c1c66d3c53e6eb66794e4b05649740e0fa606f9
Mytheus <matheussantana2603@gmail.com>
2025-02-24 23:06:07 -0300

    Finished task 03

--- tasks/task-03-get-image-info/task-03-get_image_info.py
+++ tasks/task-03-get-image-info/task-03-get_image_info.py
@@ -12,7 +12,18 @@ def get_image_info(image):
     """
     
     ### START CODE HERE ###
-    ### TODO
+    img_shp = image.shape
+    height = img_shp[0]
+    width = img_shp[1]
+    dtype = image.dtype
+    min_val = np.min(image)
+    max_val = np.max(image)
+    mean_val = np.mean(image)
+    std_val = np.std(image)
+    
+    num_channels = 1 if image.ndim == 2 else image.shape[2]
+    bits_per_channel = image.dtype.itemsize * 8
+    depth = num_channels * bits_per_channel
     ### END CODE HERE ###
 
     return {
