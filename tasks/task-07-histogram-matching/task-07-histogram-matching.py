1876e1edf455ac7deea151215e80ad5de1bd1c40
Mytheus <matheussantana2603@gmail.com>
2025-04-08 11:17:50 -0300

    Finished task 07

--- tasks/task-07-histogram-matching/task-07-histogram-matching.py
+++ tasks/task-07-histogram-matching/task-07-histogram-matching.py
@@ -26,8 +26,47 @@ Notes:
 
 import cv2 as cv
 import numpy as np
-import scikitimage as ski
+import skimage as ski
+import matplotlib.pyplot as plt
 
 def match_histograms_rgb(source_img: np.ndarray, reference_img: np.ndarray) -> np.ndarray:
     # Your implementation here
-    pass
+    return ski.exposure.match_histograms(source_img, reference_img, channel_axis=-1)
+
+def plot_images(source_img: np.ndarray, reference_img: np.ndarray, matched_img: np.ndarray):
+    fig, (ax1, ax2, ax3) = plt.subplots(
+    nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
+    for aa in (ax1, ax2, ax3):
+        aa.set_axis_off()
+
+    ax1.imshow(source_img)
+    ax1.set_title('Source')
+    ax2.imshow(reference_img)
+    ax2.set_title('Reference')
+    ax3.imshow(matched_img)
+    ax3.set_title('Matched')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_histograms(source_img: np.ndarray, reference_img: np.ndarray, matched_img: np.ndarray):
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(8, 8))
+    for i, img in enumerate((source_img, reference_img, matched_img)):
+        for c, c_color in enumerate(('red', 'green', 'blue')):
+            img_hist, bins = ski.exposure.histogram(img[..., c], source_range='dtype')
+            axes[c, i].plot(bins, img_hist / img_hist.max())
+            axes[c, 0].set_ylabel(c_color)
+    axes[0, 0].set_title('Source')
+    axes[0, 1].set_title('Reference')
+    axes[0, 2].set_title('Matched')
+
+    plt.tight_layout()
+    plt.show()
+
+def histogram_matching():
+    source = cv.imread("source.jpg")
+    reference = cv.imread("reference.jpg")
+    matched = match_histograms_rgb(source, reference)
+
+    plot_images(source, reference, matched)
+    plot_histograms(source, reference, matched)
